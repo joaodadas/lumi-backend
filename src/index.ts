d57b3b8dafd5900444037a7d344f504c9d63e4bf
Joao Vitor <dadasjv@hotmail.com>
2025-04-06 15:48:55 -0300

    ajuste

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import app from './server.js';
+import app from './server';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
